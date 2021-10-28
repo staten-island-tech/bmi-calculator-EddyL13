46f5016a29c41e2525202508a039f100ee759fde
EddyL13 <90658691+EddyL13@users.noreply.github.com>
2021-10-28 13:33:16 -0400

    ddsda

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log("connected")
+
+
+const user = {
+    name :"Dan",
+    height : "170",
+    weight : "65",};
+
+function bmi(object) {
+    weight =object.weight;
+    height= object.height/100
+    BMI=weight/height **2;
+    return BMI;}
+    console.log(bmi(user));
+    
+
+function compare (bmi, object) {
+    if (bmi>=25){
+    return `${object.name} is in the Overweight range.`; }
+    else if (bmi>=18.5) {
+     return `${object.name} is in the Healthy Weight Range.`;}
+    else if (bmi<18) {}
+        return `${object.name} is in the Underweight Range.`;}
+
+    
+
+console.log(compare(BMI,user));
